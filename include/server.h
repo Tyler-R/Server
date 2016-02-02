5b44cc5e95bc7a4d0608c7b06a3cbc0bdb73ba1c
Tyler-R <trubenui@sfu.ca>
2016-02-01 19:45:49 -0800

    Add initial project structure.

---
+++ include/server.h
@@ -0,0 +1,17 @@
+//
+// Created by tyler on 01/02/16.
+//
+
+#ifndef SERVER_SERVER_H
+#define SERVER_SERVER_H
+
+
+class Server {
+public:
+	Server();
+private:
+
+};
+
+
+#endif //SERVER_SERVER_H
---
+++ lib/server.cpp
@@ -0,0 +1,10 @@
+//
+// Created by tyler on 01/02/16.
+//
+
+#include "server.h"
+
+Server::Server() {
+
+}
+
